d43262a6f7e756b4ae8ebca0ee730f02e59ea334
Su Hang <tonyfloater@gmail.com>
2018-04-04 01:38:35 +0800

    ADT 50% (Algebraic Data Type)

--- Pyskell/Language/PyskellTypeSystem/AlgebraicDataType.py
+++ Pyskell/Language/PyskellTypeSystem/AlgebraicDataType.py
@@ -4,6 +4,7 @@ We can carefully, and easily create a lot of types
 """
 
 from TypeSignature import *
+from collections import namedtuple
 
 
 class ADT(OriginType):
@@ -17,7 +18,7 @@ def generate_type_constructor(name, type_args):
     def raise_error(whatever):
         raise whatever()
     default_attributes = {"__parameters__": tuple(type_args),
-                          "__constraints__": ()}
+                          "__constructors__": ()}
     new_type = type(name, (ADT,), default_attributes)
     new_type.__type__ = lambda self: TypeOperator(new_type,
                                                   [TypeVariable()
@@ -41,5 +42,32 @@ def generate_type_constructor(name, type_args):
     return new_type
 
 
-def generate_data_constructor():
-    pass
+def generate_data_constructor(data_con_name, fields,
+                              master_type_constructor,
+                              slot_order_in_adt):
+    base_class = namedtuple(data_con_name, ["i{}".format(i)
+                                            for i, _ in enumerate(fields)])
+    data_con_class = type(data_con_name, (master_type_constructor,
+                                          base_class), {})
+    data_con_class.__type_constructor__ = master_type_constructor
+    data_con_class.__ADT__slot__order__ = slot_order_in_adt
+    if len(fields) == 0:
+        data_con_class = data_con_class()
+    else:
+        def a_list_of_types(self):
+            type_args = [type_of(self[fields.index(k)])
+                         if k in fields else TypeVariable()
+                         for k in master_type_constructor.__parameters__]
+            return TypeOperator(master_type_constructor, type_args)
+        data_con_class.__type__ = a_list_of_types
+    master_type_constructor.__constructors__ += (data_con_class,)
+    return data_con_class
+
+
+def build_adt(typename, type_args, data_constructors, to_derive):
+    adt_type = generate_type_constructor(typename, type_args)
+    data_cons = [generate_data_constructor(dc_name, dc_field, adt_type, i)
+                 for i, (dc_name, dc_field) in enumerate(data_constructors)]
+
+    # TODO: SOMETHING NEED TO BE DONE LATER
+    return tuple([adt_type] + data_cons)
