506e5e49f3e1a7420f40f0d376120cd7105c569c
tonyfloatersu <tonyfloater@gmail.com>
2021-06-21 01:32:45 -0400

    update

--- Pyskell/Language/HindleyMilner.py
+++ Pyskell/Language/HindleyMilner.py
@@ -8,9 +8,7 @@ class HType(ABC):
     @abstractmethod
     def __name__(self) -> str: ...
 
-    @abstractmethod
-    def __repr__(self) -> str:
-        return self.__name__
+    def __repr__(self) -> str: return self.__name__
 
 
 def show_type(something):
@@ -22,16 +20,15 @@ def show_type(something):
 class TVariable(HType):
     var_id = 0
 
-    def __init__(self, constraints={}):
+    def __init__(self, constraints=None):
         self.name_ord = TVariable.var_id
-        self.name_id = 'a' + str(TVariable.var_id)
         TVariable.var_id += 1
-        self.constraints = constraints
+        self.constraints = {} if constraints is None else constraints
         self.instance = None
 
     @property
     def __name__(self) -> str:
-        return self.name_id
+        return 'a' + str(TVariable.var_id)
 
     def __repr__(self) -> str:
         return "TypeVariable({})".format(self.name_ord)
@@ -51,8 +48,8 @@ class TOperator(HType):
 
 
 class TFunction(TOperator):
-    def __init__(self, from_T: HType, to_T: HType) -> None:
-        super().__init__("->", [from_T, to_T])
+    def __init__(self, from_t: HType, to_t: HType) -> None:
+        super().__init__("->", [from_t, to_t])
 
     @property
     def __name__(self) -> str:
@@ -62,8 +59,8 @@ class TFunction(TOperator):
 
 
 class TList(TOperator):
-    def __init__(self, in_T: HType):
-        super().__init__("[]", [in_T])
+    def __init__(self, in_t: HType):
+        super().__init__("[]", [in_t])
 
     @property
     def __name__(self) -> str:
@@ -71,8 +68,8 @@ class TList(TOperator):
 
 
 class TTuple(TOperator):
-    def __init__(self, in_Ts: List[HType]):
-        super().__init__("()", in_Ts)
+    def __init__(self, in_ts: List[HType]):
+        super().__init__("()", in_ts)
 
     @property
     def __name__(self) -> str:
